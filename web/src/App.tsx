7c75c78cba4332bb77b92aba5ae7c2eccf75fbad
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-10-13 20:27:16 -0300

    use interface

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
 
+interface TitleProps {
+  
+  text: string;
+}
+
+function Title(props: TitleProps) {
+
+  return (
+    <h1>{props.text}</h1>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Title text="Titulo 1" />
+      <Title text="Titulo 2" />
+      <Title text="Titulo 3" />
+      <Title text="Titulo 4" />
     </div>
   );
 }
