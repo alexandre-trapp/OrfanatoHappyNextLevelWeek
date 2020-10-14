b5802d751d930cfd5234e670ef9fa1a7faca7731
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-10-13 21:51:30 -0300

    add css / tsx orphanages

--- web/src/pages/Landing.tsx
+++ web/src/pages/Landing.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { FiArrowRight } from 'react-icons/fi' 
+import { Link } from 'react-router-dom';
 
 import '../styles/global.css';
 import '../styles/pages/landing.css';
@@ -12,25 +13,26 @@ function Landing() {
     return (
         <div id="page-landing">
       
-        <div className="content-wrapper">
-          <img src={logoImg} alt="happy" />
+          <div className="content-wrapper">
+            <img src={logoImg} alt="happy" />
+            
+            <main>
+              <h1>Leve felicidade para o mundo</h1>
+              <p>Visite orfanatos e mude o dia muitas crianças.</p>
+            </main>
+    
+            <div className="location">
+              <strong>Blumenau</strong>
+              <span>Santa Catarina</span>
+            </div>  
+    
+            <Link to="/app" className="enter-app">
+              <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+            </Link>
+        
+          </div>
           
-          <main>
-            <h1>Leve felicidade para o mundo</h1>
-            <p>Visite orfanatos e mude o dia muitas crianças.</p>
-          </main>
-  
-          <div className="location">
-            <strong>Blumenau</strong>
-            <span>Santa Catarina</span>
-          </div>  
-  
-          <a href="" className="enter-app">
-            <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
-          </a>
         </div>
-      
-      </div>
     );
 }
 
