44141e3c90dcdb51f355155553dd75748e6aa977
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-10-14 18:14:20 -0300

    create controller

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,36 +1,10 @@
 import { Router } from 'express';
-import { getRepository } from 'typeorm';
-import Orphanage from './models/Orphanage';
+import OrphanagesController from './controllers/OrphanagesController';
 
 const routes = Router();
 
-routes.post('/orphanages', async (request, response) => {
+// MVC (Model / View / Controllers)
 
-    const { 
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends
-    } = request.body;
+routes.post('/orphanages', OrphanagesController.create);
 
-    const orphanagesRepository = getRepository(Orphanage);
-    
-    const orphanage = orphanagesRepository.create({
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends
-    });
-
-    await orphanagesRepository.save(orphanage);
-
-    return response.status(201).json(orphanage);
-});
-
-export default routes;
+export default routes;  
