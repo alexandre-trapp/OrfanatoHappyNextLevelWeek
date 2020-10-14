0d7020ecb595de35776d92c904bcac5815168c0c
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-10-14 18:22:53 -0300

    index, show orfanatos

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,7 +4,10 @@ import OrphanagesController from './controllers/OrphanagesController';
 const routes = Router();
 
 // MVC (Model / View / Controllers)
+// index, show, create, update, delete
 
+routes.get('/orphanages', OrphanagesController.index);
+routes.get('/orphanages/:id', OrphanagesController.show);
 routes.post('/orphanages', OrphanagesController.create);
 
 export default routes;  
