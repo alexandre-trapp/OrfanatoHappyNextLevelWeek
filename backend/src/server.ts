12b4ca124cac496eedd362f668c464d0bfcffa5d
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-10-15 15:29:55 -0300

    error handler

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,18 @@
 import express from 'express';
 import path from 'path';
+import 'express-async-errors';
 import './database/connection';
 
 import routes from './routes';
+import errorhandler from './errors/handler';
+import errorHandler from './errors/handler';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler);
 
 // Rota = conjunto
 // recurso = usuário
