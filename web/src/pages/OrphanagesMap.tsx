866c8c217a31dc87fc464c8bdbb4bbf4d64ceff0
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-10-15 18:01:28 -0300

    add map icon

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,48 +1,69 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi';
-import { Map, TileLayer } from 'react-leaflet';
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import Leaflet from 'leaflet';
 
 import 'leaflet/dist/leaflet.css';
 import mapMarkerImg from '../images/map-marker.svg';
 
 import '../styles/pages/orphanages-map.css';
 
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkerImg,
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2]
+})
+
 function OrphanagesMap() {
-    return (
-        <div id="page-map">
-            <aside>
-                <header>
-                    <img src={mapMarkerImg} alt="Happy" />
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+            <img src={mapMarkerImg} alt="Happy" />
 
-                    <h2>escolha um orfanato no mapa</h2>
-                    <p>Muitas crianças estão esperando a sua visita :)</p>
-                </header>
+            <h2>escolha um orfanato no mapa</h2>
+            <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
 
-                <footer>
-                    <strong>Blumenau</strong>
-                    <span>Santa Catarina</span>
-                </footer>
-            </aside>
+        <footer>
+            <strong>Blumenau</strong>
+            <span>Santa Catarina</span>
+        </footer>
+      </aside>
 
-            <Map
-                center={[-26.90228, -49.0877805]}
-                zoom={15}
-                style={{ width: '100%', height: '100%' }}
-            >
-                <TileLayer
-                    
-                    url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    /* url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} */
-                />
-            </Map>
+      <Map
+        center={[-26.90228, -49.0877805]}
+        zoom={15}
+        style={{ width: '100%', height: '100%' }}
+      >
+        <TileLayer
+            
+            url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            /* url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} */
+        />
 
-            <Link to="" className="create-orphanage">
-                <FiPlus size={32} color="#FFF" />
-            </Link>
-        
-        </div>
-    );
+        <Marker
+            icon={mapIcon}
+            position={[-26.90228, -49.0877805]}
+        >
+          <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+              Lar do ronaldo
+              <Link to="">
+                  <FiArrowRight size={20} color="#FFF" />
+              </Link>
+          </Popup>
+        </Marker>
+
+      </Map>
+
+      <Link to="" className="create-orphanage">
+          <FiPlus size={32} color="#FFF" />
+      </Link>
+    
+    </div>
+  );
 }
 
 export default OrphanagesMap;
