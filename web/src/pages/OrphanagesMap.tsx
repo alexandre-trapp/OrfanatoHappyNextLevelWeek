9328d506513d495aa5a8e2ce762ea6c8a77dfe73
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-10-15 19:50:58 -0300

    link orphanages backend to frontend

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus, FiArrowRight } from 'react-icons/fi';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 
 import mapMarkerImg from '../images/map-marker.svg';
+import mapIcon from '../utils/mapIcon';
+import api from '../services/api';
 
 import '../styles/pages/orphanages-map.css';
-import mapIcon from '../utils/mapIcon';
+
+interface Orphanage {
+  id: number;
+  latitude: number;
+  longitude: number;
+  name: string;
+}
 
 function OrphanagesMap() {
+
+  const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
+
+  useEffect(() => {
+    api.get('orphanages').then(response => {
+
+      setOrphanages(response.data);
+    });
+
+  }, []);
+
   return (
     <div id="page-map">
       <aside>
@@ -36,17 +55,24 @@ function OrphanagesMap() {
             /* url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} */
         />
 
-        <Marker
-            icon={mapIcon}
-            position={[-26.90228, -49.0877805]}
-        >
-          <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
-              Lar do ronaldo
-              <Link to="/orphanages/1">
+        {orphanages.map(orphanage => {
+          return (
+            <Marker
+              icon={mapIcon}
+              position={[orphanage.latitude, orphanage.longitude]}
+              key={orphanage.id}
+            >
+              <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                {orphanage.name}
+
+                <Link to={`/orphanages/${orphanage.id}`}>
                   <FiArrowRight size={20} color="#FFF" />
-              </Link>
-          </Popup>
-        </Marker>
+                </Link>
+              
+              </Popup>
+            </Marker>
+          )
+        })}
 
       </Map>
 
