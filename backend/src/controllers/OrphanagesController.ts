83c93ab25c4ace8916673f8d6d265980c2f3141e
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-10-15 15:10:35 -0300

    add image relations

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -8,7 +8,9 @@ export default {
       
     const orphanagesRepository = getRepository(Orphanage);
 
-    const orphanages = await orphanagesRepository.find();
+    const orphanages = await orphanagesRepository.find({
+      relations: ['images']
+    });
 
     return response.json(orphanages);
   },
@@ -19,7 +21,9 @@ export default {
 
     const orphanagesRepository = getRepository(Orphanage);
 
-    const orphanage = await orphanagesRepository.findOneOrFail(id);
+    const orphanage = await orphanagesRepository.findOneOrFail(id, {
+      relations: ['images']
+    });
 
     return response.json(orphanage);
   },
