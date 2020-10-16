390ed016109653fa5a5ff0c7be4c15529365468d
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-10-16 11:11:02 -0300

    call api to save orphanage

--- web/src/pages/CreateOrphanage.tsx
+++ web/src/pages/CreateOrphanage.tsx
@@ -1,14 +1,18 @@
 import React, { ChangeEvent, FormEvent, useState } from "react";
 import { Map, Marker, TileLayer } from 'react-leaflet';
 import { LeafletMouseEvent } from 'leaflet';
+import { useHistory } from "react-router-dom";
 
 import { FiPlus } from "react-icons/fi";
 
 import '../styles/pages/create-orphanage.css';
 import Sidebar from "../components/Sidebar";
 import mapIcon from "../utils/mapIcon";
+import api from "../services/api";
 
 export default function CreateOrphanage() {
+
+  const history = useHistory();
 
   const [position, setPosition] = useState({ latitude: 0, longitude: 0 });
   
@@ -46,10 +50,31 @@ export default function CreateOrphanage() {
     setPreviewImages(selectedImagesPreview);
   }
 
-  function handleSubmit(event: FormEvent) {
+  async function handleSubmit(event: FormEvent) {
 
     event.preventDefault();
 
+    const { latitude, longitude } = position;
+
+    const data = new FormData();
+
+    data.append('name', name);
+    data.append('about', about);
+    data.append('latitude', String(latitude));
+    data.append('longitude', String(longitude));
+    data.append('instructions', instructions);
+    data.append('opening_hours', opening_hours);
+    data.append('open_on_weekends', String(open_on_weekends));
+
+    images.forEach(image => {
+      data.append('images', image);
+    });
+
+    await api.post('orphanages', data);
+
+    alert('Cadastro realizado com sucesso');
+
+    history.push('/app');
   }
 
   return (
