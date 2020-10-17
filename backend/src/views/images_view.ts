0dfd40a75551aa16e8400396f3c0f3385bfd5620
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-10-17 15:37:25 -0300

    change ip from expo ip

--- backend/src/views/images_view.ts
+++ backend/src/views/images_view.ts
@@ -6,7 +6,7 @@ export default {
       
     return {  
       id: image.id,
-      url: `http://localhost:3333/uploads/${image.path}`
+      url: `http://192.168.100.3:3333/uploads/${image.path}`
     };
   },
 
