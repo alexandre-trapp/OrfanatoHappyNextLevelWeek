579d958e0ae21920cca14411e07ab3f355c19868
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-10-17 14:49:48 -0300

    add gobak button

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import { BorderlessButton } from 'react-native-gesture-handler';
 import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
 interface HeaderProps {
   title: string;
@@ -9,14 +10,24 @@ interface HeaderProps {
 
 export default function Header({ title }: HeaderProps) {
   
+  const navigation = useNavigation();
+
+  function handleGoBackToAppHomePage() {
+    navigation.navigate('OrphanagesMap');
+  }
+
   return (
     
     <View style={styles.container}>
-      <BorderlessButton onPress={() => {}}>
+      <BorderlessButton onPress={navigation.goBack}>
         <Feather name="arrow-left" size={24} color="#15b5d6" />
       </BorderlessButton>
       
       <Text style={styles.title}>{title}</Text>
+
+      <BorderlessButton onPress={handleGoBackToAppHomePage}>
+        <Feather name="x" size={24} color="#ff669d" />
+      </BorderlessButton>
     </View>
   );
 }
