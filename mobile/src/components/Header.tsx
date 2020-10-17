c220aeb441abd6771c0be592df95f15c271cccbe
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-10-17 14:46:17 -0300

    add rectButton

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import { BorderlessButton } from 'react-native-gesture-handler';
+import { Feather } from '@expo/vector-icons';
 
 interface HeaderProps {
   title: string;
 }
 
 export default function Header({ title }: HeaderProps) {
+  
   return (
+    
     <View style={styles.container}>
+      <BorderlessButton onPress={() => {}}>
+        <Feather name="arrow-left" size={24} color="#15b5d6" />
+      </BorderlessButton>
+      
       <Text style={styles.title}>{title}</Text>
     </View>
   );
